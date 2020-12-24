abd373da3b3c65e0678a8583e147d9665992e9c4
CloudStriker <raden.michael@ti.ukdw.ac.id>
2020-12-24 14:03:16 +0900

    Fixing Some Code
    Release?

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,16 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'package:convex_bottom_bar/convex_bottom_bar.dart';
-void main() {
-  runApp(MyApp());
-}
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Splash Screen',
+      title: 'Yorsha',
       theme: ThemeData(
-        primarySwatch: Colors.green,
+        primarySwatch: Colors.blue,
       ),
       home: MyHomePage(),
       debugShowCheckedModeBanner: false,
@@ -24,33 +23,28 @@ class MyHomePage extends StatefulWidget {
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
+
 class _MyHomePageState extends State<MyHomePage> {
   @override
   void initState() {
     super.initState();
-    Timer(Duration(seconds: 3),
-            ()=>Navigator.pushReplacement(context,
-            MaterialPageRoute(builder:
-                (context) =>
-                SecondScreen()
-            )
-        )
-    );
+    Timer(
+        Duration(seconds: 3),
+        () => Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => SecondScreen())));
   }
+
   @override
   Widget build(BuildContext context) {
     return new DecoratedBox(
       decoration: new BoxDecoration(
         image: new DecorationImage(
-            image: new AssetImage('assets/splash.png'),
-            fit: BoxFit.fill
-
-        ),
+            image: new AssetImage('assets/splash.png'), fit: BoxFit.fill),
       ),
     );
-
   }
 }
+
 class SecondScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
